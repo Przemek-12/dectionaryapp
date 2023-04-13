2f0341563a5429b163c7ef30a147ea57b2c883c0
Przemyslaw_Chlebek <przemyslaw.chlebek@hycom.pl>
2023-04-13 18:06:36 +0200

    dict

--- src/main/java/com/app/dictionary/infrastructure/DictionaryApplication.java
+++ src/main/java/com/app/dictionary/infrastructure/DictionaryApplication.java
@@ -1,4 +1,4 @@
-package com.app.dictionary;
+package com.app.dictionary.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/app/dictionary/presentation/DictionaryController.java
@@ -0,0 +1,39 @@
+package com.app.dictionary.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/dictionary")
+@RestController
+@RequiredArgsConstructor
+public class DictionaryController {
+
+    public Object translate() {
+
+    }
+
+    public Object translateSentence() {
+
+    }
+
+    public Object addWord() {
+
+    }
+
+    public List<DictionaryListResponse> getAll() {
+
+    }
+
+    public Object getReport() {
+
+    }
+
+    public Object getReportAsPDF() {
+
+    }
+
+
+
+
+}
